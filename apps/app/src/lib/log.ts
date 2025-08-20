2ada350feed348243e401babe704865ad3a1ba3a
Mikhail Garbuzov <mikhail.garbuzov@ringcentral.com>
2025-08-20 23:15:09 +0200

    pino hook with winston style

--- apps/app/src/lib/log.ts
+++ apps/app/src/lib/log.ts
@@ -9,6 +9,21 @@ const loggerOptions: LoggerOptions = {
     err: pino.stdSerializers.errWithCause,
     error: pino.stdSerializers.errWithCause,
   },
+  hooks: {
+    logMethod(inputArgs, method, _levelNumber) {
+      if (
+        inputArgs.length >= 2 &&
+        typeof inputArgs[0] === 'string' &&
+        typeof inputArgs[1] === 'object'
+      ) {
+        // swap the first two arguments, resulting in `(context, message)`
+        const args = inputArgs as unknown[];
+        [args[0], args[1]] = [args[1], args[0]];
+      }
+
+      return method.apply(this, inputArgs);
+    },
+  },
 };
 
 export const rootLogger = pino(loggerOptions);
