32f58f5e85a87fa2dcc224aa61af2c22a8565eac
Mikhail Garbuzov <mikhail.garbuzov@ringcentral.com>
2025-08-20 23:15:37 +0200

    pass redis instance

--- apps/app/src/middlewares/populate-store.ts
+++ apps/app/src/middlewares/populate-store.ts
@@ -1,11 +1,12 @@
 import { randomUUID } from 'node:crypto';
 import { type NextFunction, type Request, type Response } from 'express';
 
-import { RDocumentLoader } from '../lib/RDocumentLoader';
+import { RDocumentLoader } from '../lib/RDocumentLoader/RDocumentLoader';
 import { setStoreValue } from '../lib/request-store';
+import { redis } from '../lib/redis';
 
 export function populateStoreMiddleware(req: Request, _res: Response, next: NextFunction): void {
   setStoreValue('requestId', req.get('x-request-id') ?? randomUUID());
-  setStoreValue('loader', RDocumentLoader.createInstance());
+  setStoreValue('loader', RDocumentLoader.createInstance(redis));
   next();
 }
