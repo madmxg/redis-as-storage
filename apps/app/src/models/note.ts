102aa20d8d21b5f6c7231e891c7e5792127da4a7
Mikhail Garbuzov <mikhail.garbuzov@ringcentral.com>
2025-09-03 00:43:10 +0200

    route /notes

---
+++ apps/app/src/models/note.ts
@@ -0,0 +1,65 @@
+import assert from 'node:assert/strict';
+
+import { type RDocument } from '../lib/r-document';
+import { type RLoader } from '../lib/r-loader';
+
+export type NoteDocument = {
+  id: string;
+  text?: string;
+};
+
+export class Note implements RDocument {
+  #exist = false;
+  #deleted = false;
+
+  public readonly id: string;
+  public text: string | undefined;
+
+  constructor(id: string) {
+    this.id = id;
+  }
+
+  prepareLoad(loader: RLoader): void {
+    loader.enqueueCommand<string>('GET', `${this.id}:text`, (result) => {
+      if (typeof result === 'string') {
+        this.#exist = true;
+        this.text = result;
+      }
+    });
+  }
+
+  prepareSave(loader: RLoader): void {
+    if (typeof this.text !== 'undefined') {
+      loader.enqueueCommand<'OK'>('SET', `${this.id}:text`, this.text, (result) => {
+        if (result === 'OK') {
+          this.#exist = true;
+        }
+      });
+    }
+  }
+
+  prepareDelete(loader: RLoader): void {
+    loader.enqueueCommand<1>('DEL', `${this.id}:text`, (result) => {
+      this.#deleted = result === 1;
+    });
+  }
+
+  getDocument(): NoteDocument {
+    return {
+      id: this.id,
+      text: this.text,
+    };
+  }
+
+  get exist(): boolean {
+    return this.#exist;
+  }
+
+  get deleted(): boolean {
+    return this.#deleted;
+  }
+
+  async postLoad(): Promise<void> {}
+  async postSave(): Promise<void> {}
+  async postDelete(): Promise<void> {}
+}
